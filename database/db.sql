75f728f1f8f465fb4082362cf6b37feaedd94e92
Adonis Aleman <adonis.aleman@unadeca.net>
2022-10-25 11:49:31 -0600

    Cambios en la base de datos

--- database/db.sql
+++ database/db.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE adluis;
 
-CREATE TABLE user(
+CREATE TABLE userr(
     id_s SERIAL PRIMARY KEY,
     s_username TEXT NOT NULL,
     s_email TEXT NOT NULL UNIQUE,
@@ -10,7 +10,7 @@ CREATE TABLE user(
 /* regitro de cuenta bancaria */
 CREATE TABLE rcuentabancaria(
     id_r SERIAL PRIMARY KEY,
-    r_id INTEGER REFERENCES user(id_s)
+    r_id INTEGER REFERENCES userr(id_s),
     r_namebanco TEXT NOT NULL,
     r_numcuenta TEXT NOT NULL,
     r_url TEXT NOT NULL
@@ -19,7 +19,7 @@ CREATE TABLE rcuentabancaria(
 /* registro de monto */
 CREATE TABLE montoregistro(
     id_m SERIAL PRIMARY KEY,
-    m_id INTEGER REFERENCES rcuentabancaria(id_r)
+    m_id INTEGER REFERENCES rcuentabancaria(id_r),
     m_banco TEXT NOT NULL,
     m_monto TEXT NOT NULL,
     m_detalle TEXT NOT NULL
@@ -36,7 +36,7 @@ CREATE TABLE gastoregistro(
 /*Regitro de pagos*/
 CREATE TABLE pagosregistro(
     id_p SERIAL PRIMARY KEY,
-    p_id INTEGER REFERENCES gastoregistro(id_g)
+    p_id INTEGER REFERENCES gastoregistro(id_g),
     p_monto TEXT NOT NULL,
     p_descriocion TEXT NOT NULL
 
