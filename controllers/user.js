9f5dc1ae75c601e96fa82253de2c27a0b68f34be
Adonis Aleman <adonis.aleman@unadeca.net>
2022-10-25 18:09:31 -0600

    subiendo aprte registro de monto

--- controllers/user.js
+++ controllers/user.js
@@ -66,6 +66,74 @@ user.updateRcuentabancaria = async (req, res) => {
   }
 };
 
+//post monto registro
+user.createMontoregistro = async (req, res) => {
+  const { id, m_banco, m_monto, m_detalle } = req.body;
+  try {
+    await pool.query(
+      "INSERT INTO montoregistro (m_id, m_banco, m_monto, m_detalle) VALUES ($1, $2, $3, $4)",
+      [id, m_banco, m_monto, m_detalle]
+    );
+    res.status(200).json({
+      message: "Se creo el monto con exito",
+      body: {
+        id,
+        m_banco,
+        m_monto,
+        m_detalle,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Algo salio mal, intentelo de nuevo",
+      error,
+    });
+  }
+};
+
+//get monto registro
+user.readMontoregistro = async (req, res) => {
+  const id = req.params.id_m;
+  try {
+    const montoregistro = await await pool.query(
+      "SELECT * FROM montoregistro WHERE id_m = $1",
+      [id]
+    );
+    res.status(200).json({ montoregistro });
+  } catch (error) {
+    res.status(500).json({
+      message: "A ocurrido un error",
+      error,
+    });
+  }
+};
+
+//put monto registro
+user.updateMontoregistro = async (req, res) => {
+  const id = req.params.id_m;
+  const { m_banco, m_monto, m_detalle } = req.body;
+  try {
+    await pool.query(
+      "UPDATE montoregistro SET m_banco = $1, m_monto = $2, m_detalle = $3 WHERE id_m = $4",
+      [m_banco, m_monto, m_detalle, id]
+    );
+    res.status(200).json({
+      message: "Se actualizo el monto con exito",
+      body: {
+        id,
+        m_banco,
+        m_monto,
+        m_detalle,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "A ocurrido un error",
+      error,
+    });
+  }
+};
+
 //reguistro de gastos
 //--------------------
 user.createGastoregistro = async (req, res) => {
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -11,6 +11,14 @@ router.get("/rcuentabancaria/:id_r", user.readRcuentabancaria);
 
 router.put("/rcuentabancaria/:id_t", user.updateRcuentabancaria);
 
+// rutas de montoregistro
+
+router.post("/montoregistro", user.createMontoregistro);
+
+router.get("/montoregistro/:id", user.readMontoregistro);
+
+router.put("/montoregistro/:id", user.updateMontoregistro);
+
 // rutas gastoregistro
 
 router.post("/gastoregistro", user.createGastoregistro);
